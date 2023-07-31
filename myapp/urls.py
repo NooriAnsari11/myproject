0e7f9562fdc06f2f218f19172ec5e81a68419cd2
Noori <vdnr38@durham.ac.uk>
2023-07-31 22:00:49 +0100

    added gunicorn

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from  . import views
 urlpatterns = [
     path('',views.index,name='index'),
@@ -31,3 +32,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
